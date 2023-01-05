59069979995e3ac74ba89e1f5901fc1041fefa40
shawnbirring <shawnbirring21@gmail.com>
2023-01-04 20:02:38 -0800

    removed "interesting" method, draw method draws a house with two windows and a door

--- src/main/java/org/bcit/comp2522/labs/lab01/Window.java
+++ src/main/java/org/bcit/comp2522/labs/lab01/Window.java
@@ -16,22 +16,17 @@ public class Window extends PApplet {
 
   public void draw() {
     ellipse(mouseX, mouseY, 50, 50);
+    rect(125, 250, 250, 300);
+    triangle(125, 250, 375, 250, 250, 100);
+    rect(150, 300, 100, 150);
+    rect(300,400,50,100);
+    circle(325, 300, 45);
   }
 
   public void mousePressed() {
     background(64);
   }
 
-  /**
-   * Interesting method draws some shapes.
-   */
-  public void interesting() {
-    size(250, 250);
-    line(50, 50, 100, 100);
-    triangle(10, 10, 20, 20, 30, 10);
-    rect(50, 100, 50, 75);
-    circle(50, 50, 10);
-  }
 
   /**
    * Main method runs the program.
